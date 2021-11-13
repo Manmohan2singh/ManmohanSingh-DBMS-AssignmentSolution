84211360211648dfd5ba31baff6779dc3487e5c4
Manmohan2singh <70766679+Manmohan2singh@users.noreply.github.com>
2021-11-13 15:22:34 +0530

    first edit

--- category_procedures.sql
+++ category_procedures.sql
@@ -2,7 +2,7 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `category`()
 BEGIN
 select price.distance,price.price,
 Case
-	when price.price> 1000 then 'Expensive'
+    when price.price> 1000 then 'Expensive'
     when price.price <1000 and price.price >500 then 'Average'
     Else 'Cheap'
 End as price from price;
